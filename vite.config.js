6feff1e98f4eb5d32c6feeae1b97103fc7542aae
Mezes131 <ellipsecorp35@gmail.com>
2025-12-12 04:01:51 +0100

    fix (build): fix building issues

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.MODE === 'production' 
+  base: process.env.NODE_ENV === 'production' 
     ? '/christmass-tree/'
     : '/',
 })
